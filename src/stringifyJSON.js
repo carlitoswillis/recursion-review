4fd40c78ce7dacff134cde78ec9efcf12717d33b
Benjamin Barnett <ben.barnett@gmail.com>
2020-03-23 11:33:46 -0700

    Implement string, boolean, and array cases
    Arrays are our first recursive case, not yet tested.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,26 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  // base cases: numbers, strings, booleans
+  // recursive cases: arrays and objects
+  let resultString = '';
+
+  if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+
+  if (typeof obj === 'boolean'){
+    return obj + '';
+  }
+
+  if (Array.isArray(obj)){
+    for (let i = 0; i < obj.length - 1; i++){
+      resultString += stringifyJSON(obj[i]) + ',';
+    }
+    resultString += stringifyJSON(obj[obj.length - 1]);
+  }
+
+  return resultString;
+
   // your code goes here
 };
