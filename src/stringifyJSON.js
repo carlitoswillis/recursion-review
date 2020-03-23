b74e88b24745e42e325775c1d673bed1cf03a5f6
Benjamin Barnett <ben.barnett@gmail.com>
2020-03-23 11:57:30 -0700

    Implement object case, fix test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,21 +8,31 @@ var stringifyJSON = function(obj) {
   // recursive cases: arrays and objects
   let resultString = '';
 
-  if (typeof obj === 'string'){
+  if (typeof obj === 'string') {
     return '"' + obj + '"';
   }
 
-  if (typeof obj === 'boolean' || typeof obj === 'number'){
+  if (typeof obj === 'boolean' || typeof obj === 'number') {
     return obj + '';
   }
 
-  if (Array.isArray(obj)){
+  if (Array.isArray(obj)) {
     resultString += '[';
-    for (let i = 0; i < obj.length - 1; i++){
+    for (let i = 0; i < obj.length - 1; i++) {
       resultString += stringifyJSON(obj[i]) + ',';
     }
     resultString += stringifyJSON(obj[obj.length - 1]);
     resultString += ']';
+  }
+
+  if (typeof obj === 'object') {
+    resultString += '{';
+    for (var key in obj) {
+      resultString += stringifyJSON(key) + ':';
+      resultString += stringifyJSON(obj[key]);
+    }
+
+    resultString += '}';
   }
 
   return resultString;
